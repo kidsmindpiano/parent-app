f4f1f80c807011e0b0f3645a3c8bd18c0c448838
jihye <jihye@Mac.Davolink>
2026-03-11 10:47:09 +0900

    레이아웃 여백 추가 - 화면 꽉차지 않게

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,7 @@ import BottomNav from './BottomNav';
 export default function Layout() {
   return (
     <div className="min-h-screen bg-bg">
-      <main className="max-w-lg md:max-w-2xl lg:max-w-4xl mx-auto pb-24 px-5 md:px-8 pt-5 md:pt-6">
+      <main className="max-w-md md:max-w-xl lg:max-w-3xl mx-auto pb-24 px-6 md:px-8 pt-5 md:pt-6">
         <Outlet />
       </main>
       <BottomNav />
